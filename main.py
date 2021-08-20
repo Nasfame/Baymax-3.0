0e5a3f76a1d5bddc58e69557190239850fa4456b
Hiro <laciferin@gmail.com>
2021-08-21 03:37:32 +0530

    Rest

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-from JarvisAI import
+from JarvisAI import JarvisAssistant
+
+baymax = JarvisAssistant(sync=True,token="436c57eab581dbb2253cfa77c41574f6")
+
+while 1:
+    res = baymax.mic_input(lang='en')
+    print(res)
